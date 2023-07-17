cc3130b8b735aab4e7d6e1aa0a5c003da2d0da47
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-07-17 20:14:08 +0600

    added factory listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,30 +1,11 @@
-<?php 
-    namespace App\Models;
+<?php
 
-    class Listing{
-        public static function all(){
-            return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, corrupti fugit fuga amet inventore omnis nostrum laborum, excepturi delectus suscipit, dignissimos quis molestias saepe neque rerum. Explicabo, eum nam itaque, facere cumque voluptatem a dolorum blanditiis totam ut numquam provident ducimus, omnis molestiae! Quae id modi nam error, omnis doloremque?"
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, corrupti fugit fuga amet inventore omnis nostrum laborum, excepturi delectus suscipit, dignissimos quis molestias saepe neque rerum. Explicabo, eum nam itaque, facere cumque voluptatem a dolorum blanditiis totam ut numquam provident ducimus, omnis molestiae! Quae id modi nam error, omnis doloremque?"
-            ]
-            ];
-        }
+namespace App\Models;
 
-        public static function find($id){
-            $listings = self::all();
-            
-            foreach ($listings as $listing) {
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-        }
-    }
-?>
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel,php,backend,fullstack',
+            'company' => 'Acme Corp',
+            'location' => 'Remote',
+            'email' => 'email@email.com',
+            'website' => 'https://example.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda sequi consequatur est. Sapiente eaque repudiandae commodi quisquam voluptates non, nisi corporis, omnis fugit odio similique delectus dolores impedit, ipsa a molestiae doloremque magnam cupiditate ut eum tempora! Nisi totam in iusto odio, aliquid voluptate cupiditate quod iste facilis est enim!'
+        ]);
+
+        Listing::create([
+            'title' => 'Full Stack Developer',
+            'tags' => 'laravel,php,backend,fullstack',
+            'company' => 'Acme Corp',
+            'location' => 'Remote',
+            'email' => 'email@email.com',
+            'website' => 'https://example.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda sequi consequatur est. Sapiente eaque repudiandae commodi quisquam voluptates non, nisi corporis, omnis fugit odio similique delectus dolores impedit, ipsa a molestiae doloremque magnam cupiditate ut eum tempora! Nisi totam in iusto odio, aliquid voluptate cupiditate quod iste facilis est enim!'
+        ]);
+
+        Listing::factory(10)->create();
+        
     }
 }
