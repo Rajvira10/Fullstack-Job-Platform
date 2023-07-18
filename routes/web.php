067cf1c7a59942592c42cf46c629c47e308e9333
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-07-18 15:48:57 +0600

    added search and filter functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,16 +16,7 @@ use App\Models\Listing;
 */
 
 //All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Job Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //Single Listing
-Route::get('/listings/{id}', function ($id) {
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
