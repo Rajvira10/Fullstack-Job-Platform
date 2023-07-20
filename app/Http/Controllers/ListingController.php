dd9298f6011cd752a0a2f3a705e7b60ef5fa6698
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-07-20 19:26:18 +0600

    image upload done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -35,6 +35,9 @@ class ListingController extends Controller
             'tags' => 'required'
         ]);
 
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
         Listing::create($formFields);
         
         return redirect('/')->with('message','Listing created successfully!');
